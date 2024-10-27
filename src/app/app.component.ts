fb27f1a0508e704c7f81ec6acdfe36ea4ebc72d5
begal <benjamin.galaud45@gmail.com>
2024-10-27 10:51:27 +0100

    test img 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,6 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'Leïla DEVESSE (LD Juris)';
   description ='Facilitez votre quotidien en confiant vos formalités juridiques !';
-  pp = '/assets/ppLinkedIn.jpg';
-  bkg = '/assets/bkgLinkedIn.jpg';
+  pp = 'https://media.licdn.com/dms/image/v2/D4E03AQHk8CE315rn3A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1696840332649?e=1735776000&v=beta&t=cxdnpJlulntGt93m9uZ2Yf_l9nQ0KmqRktgA8wJeFSQ';
+  bkg = 'https://media.licdn.com/dms/image/v2/D4E16AQHaWjMqvYdstQ/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1706205551729?e=1735776000&v=beta&t=f4SN-yfPkj2VOdGlqZgcLauwf6fZG3nYokhjG2C3Q2s';
 }
