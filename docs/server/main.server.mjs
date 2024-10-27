8a761ae9e2dd0619476136bf18cf0f9f563a7cb8
begal <benjamin.galaud45@gmail.com>
2024-10-27 11:09:12 +0100

    add links

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KWN5H2S6.mjs";import"./chunk-J4RVASKU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-MB3K23VX.mjs";import"./chunk-U4RCMA6M.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
