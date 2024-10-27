64a3dc0dd47aab56ee2cabe12cb7ed514226f647
begal <benjamin.galaud45@gmail.com>
2024-10-27 10:43:25 +0100

    test img 2

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-E4T3KY2W.mjs";import"./chunk-GTLE74PO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-SD3WYCDZ.mjs";import"./chunk-J4RVASKU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
