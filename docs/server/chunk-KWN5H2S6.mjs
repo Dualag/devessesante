510853a36217048cf179ecdf2ff3fcb976922f62
begal <benjamin.galaud45@gmail.com>
2024-10-27 10:55:07 +0100

    test img 5

---
+++ docs/server/chunk-KWN5H2S6.mjs
@@ -0,0 +1,169 @@
+import './polyfills.server.mjs';
+import{A as w,D as P,H as q,K as y,a as h,b as u,c as m,d as p,e as o,f as g,g as f,h as C,i as e,j as n,k as a,l as c,m as d,n as b,o as s,p as M,u as _,x as k,z as x}from"./chunk-J4RVASKU.mjs";var D=(i,r)=>r.title,I=()=>({title:"Explore the Docs",link:"https://angular.dev"}),H=()=>({title:"Learn with Tutorials",link:"https://angular.dev/tutorials"}),L=()=>({title:"CLI Docs",link:"https://angular.dev/tools/cli"}),T=()=>({title:"Angular Language Service",link:"https://angular.dev/tools/language-service"}),U=()=>({title:"Angular DevTools",link:"https://angular.dev/tools/devtools"}),z=(i,r,t,l,E)=>[i,r,t,l,E];function F(i,r){if(i&1&&(e(0,"a",7)(1,"span"),c(2),n(),u(),e(3,"svg",18),a(4,"path",19),n()()),i&2){let t=r.$implicit;g("href",t.link,p),o(2),d(t.title)}}var v=class i{title="Le\xEFla DEVESSE (LD Juris)";description="Facilitez votre quotidien en confiant vos formalit\xE9s juridiques !";pp="https://media.licdn.com/dms/image/v2/D4E03AQHk8CE315rn3A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1696840332649?e=1735776000&v=beta&t=cxdnpJlulntGt93m9uZ2Yf_l9nQ0KmqRktgA8wJeFSQ";bkg="https://media.licdn.com/dms/image/v2/D4E16AQHaWjMqvYdstQ/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1706205551729?e=1735776000&v=beta&t=f4SN-yfPkj2VOdGlqZgcLauwf6fZG3nYokhjG2C3Q2s";static \u0275fac=function(t){return new(t||i)};static \u0275cmp=h({type:i,selectors:[["app-root"]],standalone:!0,features:[b],decls:27,vars:15,consts:[[1,"main"],[1,"content"],[1,"left-side"],[3,"src"],["role","separator","aria-label","Divider",1,"divider"],[1,"right-side"],[1,"pill-group"],["target","_blank","rel","noopener",1,"pill",3,"href"],[1,"social-links"],["href","https://github.com/angular/angular","aria-label","Github","target","_blank","rel","noopener"],["width","25","height","24","viewBox","0 0 25 24","fill","none","xmlns","http://www.w3.org/2000/svg","alt","Github"],["d","M12.3047 0C5.50634 0 0 5.50942 0 12.3047C0 17.7423 3.52529 22.3535 8.41332 23.9787C9.02856 24.0946 9.25414 23.7142 9.25414 23.3871C9.25414 23.0949 9.24389 22.3207 9.23876 21.2953C5.81601 22.0377 5.09414 19.6444 5.09414 19.6444C4.53427 18.2243 3.72524 17.8449 3.72524 17.8449C2.61064 17.082 3.81137 17.0973 3.81137 17.0973C5.04697 17.1835 5.69604 18.3647 5.69604 18.3647C6.79321 20.2463 8.57636 19.7029 9.27978 19.3881C9.39052 18.5924 9.70736 18.0499 10.0591 17.7423C7.32641 17.4347 4.45429 16.3765 4.45429 11.6618C4.45429 10.3185 4.9311 9.22133 5.72065 8.36C5.58222 8.04931 5.16694 6.79833 5.82831 5.10337C5.82831 5.10337 6.85883 4.77319 9.2121 6.36459C10.1965 6.09082 11.2424 5.95546 12.2883 5.94931C13.3342 5.95546 14.3801 6.09082 15.3644 6.36459C17.7023 4.77319 18.7328 5.10337 18.7328 5.10337C19.3942 6.79833 18.9789 8.04931 18.8559 8.36C19.6403 9.22133 20.1171 10.3185 20.1171 11.6618C20.1171 16.3888 17.2409 17.4296 14.5031 17.7321C14.9338 18.1012 15.3337 18.8559 15.3337 20.0084C15.3337 21.6552 15.3183 22.978 15.3183 23.3779C15.3183 23.7009 15.5336 24.0854 16.1642 23.9623C21.0871 22.3484 24.6094 17.7341 24.6094 12.3047C24.6094 5.50942 19.0999 0 12.3047 0Z"],["href","https://twitter.com/angular","aria-label","Twitter","target","_blank","rel","noopener"],["width","24","height","24","viewBox","0 0 24 24","fill","none","xmlns","http://www.w3.org/2000/svg","alt","Twitter"],["d","M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"],["href","https://www.youtube.com/channel/UCbn1OgGei-DV7aSRo_HaAiw","aria-label","Youtube","target","_blank","rel","noopener"],["width","29","height","20","viewBox","0 0 29 20","fill","none","xmlns","http://www.w3.org/2000/svg","alt","Youtube"],["fill-rule","evenodd","clip-rule","evenodd","d","M27.4896 1.52422C27.9301 1.96749 28.2463 2.51866 28.4068 3.12258C29.0004 5.35161 29.0004 10 29.0004 10C29.0004 10 29.0004 14.6484 28.4068 16.8774C28.2463 17.4813 27.9301 18.0325 27.4896 18.4758C27.0492 18.9191 26.5 19.2389 25.8972 19.4032C23.6778 20 14.8068 20 14.8068 20C14.8068 20 5.93586 20 3.71651 19.4032C3.11363 19.2389 2.56449 18.9191 2.12405 18.4758C1.68361 18.0325 1.36732 17.4813 1.20683 16.8774C0.613281 14.6484 0.613281 10 0.613281 10C0.613281 10 0.613281 5.35161 1.20683 3.12258C1.36732 2.51866 1.68361 1.96749 2.12405 1.52422C2.56449 1.08095 3.11363 0.76113 3.71651 0.596774C5.93586 0 14.8068 0 14.8068 0C14.8068 0 23.6778 0 25.8972 0.596774C26.5 0.76113 27.0492 1.08095 27.4896 1.52422ZM19.3229 10L11.9036 5.77905V14.221L19.3229 10Z"],["xmlns","http://www.w3.org/2000/svg","height","14","viewBox","0 -960 960 960","width","14","fill","currentColor"],["d","M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"]],template:function(t,l){t&1&&(e(0,"main",0)(1,"div",1)(2,"div",2),a(3,"img",3)(4,"img",3),e(5,"h1"),c(6),n(),e(7,"p"),c(8),n(),e(9,"p"),c(10,`
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at molestie dolor, a convallis dui. Morbi at dui nisi. Nullam vitae mattis erat. Ut commodo finibus mauris, id pellentesque erat. Quisque ultricies lorem nec velit vulputate, quis malesuada lorem luctus. Aliquam arcu urna, semper feugiat venenatis vitae, mollis quis erat. Aenean at interdum nisi, consectetur finibus arcu. Donec rutrum ac tortor id condimentum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec at urna aliquam, vehicula tellus eu, maximus mauris. Duis ipsum enim, sollicitudin quis nibh id, rhoncus molestie turpis. Donec ut massa lacus. Suspendisse molestie a erat et ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi at tristique nulla. Mauris condimentum, lorem quis faucibus iaculis, diam urna sagittis ante, porta congue mauris nulla eget metus. Morbi a mattis libero. Ut ut justo fringilla magna blandit eleifend non rutrum purus. Curabitur ut lectus dictum magna ornare tincidunt vel a magna. Nullam ut mattis felis, et convallis metus. Nullam consectetur nisi eu efficitur ultricies. Curabitur commodo aliquam nisl, sit amet tincidunt mi interdum in. In vel odio ut velit luctus pharetra eget vitae lacus. Ut auctor feugiat lectus iaculis congue. Cras porta diam eget gravida convallis. Nam pellentesque vehicula mi dictum feugiat. Vestibulum ex leo, rutrum sit amet tellus porta, condimentum mattis elit. Etiam nec mauris eu turpis cursus accumsan. Mauris at mauris eget elit semper aliquet. Morbi commodo posuere lectus, eget fermentum risus egestas nec. Curabitur faucibus sagittis lorem, et lacinia neque efficitur vel. Maecenas nec felis ornare odio gravida scelerisque. Curabitur ut ultricies neque, in finibus magna. Integer placerat hendrerit nulla. Praesent sodales scelerisque rhoncus. Sed vestibulum libero tellus, a posuere leo pretium in. Sed maximus consequat magna, ut condimentum dolor lobortis at. Phasellus feugiat id arcu nec porta. Ut vitae rhoncus quam, ac tincidunt lacus. Donec pharetra tellus lectus, pretium consequat mauris cursus at. Mauris fringilla varius neque ut scelerisque. In varius metus vel odio venenatis tempor. Curabitur luctus arcu ex, nec molestie libero facilisis in. Phasellus lorem ipsum, tempus a metus et, vestibulum tincidunt felis. Maecenas pharetra, odio nec dapibus bibendum, neque erat bibendum metus, sed congue lorem velit a enim. Quisque quis porta lorem. Duis nec est vitae metus varius ultricies. Morbi fermentum neque nec velit scelerisque, eu congue quam eleifend. Aenean pretium convallis hendrerit. Pellentesque at nunc sapien. Nullam nec lorem lobortis, suscipit nulla at, tincidunt massa. Integer feugiat ullamcorper eros vitae posuere. Etiam ac nulla ut purus luctus faucibus. Pellentesque ante ex, sollicitudin vitae fermentum at, blandit nec nisi. Duis porta quam ac erat gravida, volutpat pulvinar lacus auctor. Aenean fringilla diam et cursus scelerisque. Proin fermentum ornare blandit. Morbi urna quam, venenatis eget velit quis, interdum maximus ligula. Maecenas non luctus odio, vel feugiat ex. Mauris auctor leo non ligula sollicitudin, sodales egestas sem bibendum. Integer suscipit iaculis varius. Duis ac fermentum velit, a faucibus tortor. Cras consectetur laoreet enim, in consequat erat egestas vel. Integer non nunc feugiat, lacinia ex non, dignissim erat. `),n()(),a(11,"div",4),e(12,"div",5)(13,"div",6),f(14,F,5,2,"a",7,D),n(),e(16,"div",8)(17,"a",9),u(),e(18,"svg",10),a(19,"path",11),n()(),m(),e(20,"a",12),u(),e(21,"svg",13),a(22,"path",14),n()(),m(),e(23,"a",15),u(),e(24,"svg",16),a(25,"path",17),n()()()()()(),m(),a(26,"router-outlet")),t&2&&(o(3),g("src",l.bkg,p),o(),g("src",l.pp,p),o(2),d(l.title),o(2),d(l.description),o(6),C(M(9,z,s(4,I),s(5,H),s(6,L),s(7,T),s(8,U))))},dependencies:[q],styles:[`[_nghost-%COMP%] {
+    --bright-blue: oklch(51.01% 0.274 263.83);
+    --electric-violet: oklch(53.18% 0.28 296.97);
+    --french-violet: oklch(47.66% 0.246 305.88);
+    --vivid-pink: oklch(69.02% 0.277 332.77);
+    --hot-red: oklch(61.42% 0.238 15.34);
+    --orange-red: oklch(63.32% 0.24 31.68);
+
+    --gray-900: oklch(19.37% 0.006 300.98);
+    --gray-700: oklch(36.98% 0.014 302.71);
+    --gray-400: oklch(70.9% 0.015 304.04);
+
+    --red-to-pink-to-purple-vertical-gradient: linear-gradient(
+      180deg,
+      var(--orange-red) 0%,
+      var(--vivid-pink) 50%,
+      var(--electric-violet) 100%
+    );
+
+    --red-to-pink-to-purple-horizontal-gradient: linear-gradient(
+      90deg,
+      var(--orange-red) 0%,
+      var(--vivid-pink) 50%,
+      var(--electric-violet) 100%
+    );
+
+    --pill-accent: var(--bright-blue);
+
+    font-family: "Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  h1[_ngcontent-%COMP%] {
+    font-size: 3.125rem;
+    color: var(--gray-900);
+    font-weight: 500;
+    line-height: 100%;
+    letter-spacing: -0.125rem;
+    margin: 0;
+    font-family: "Inter Tight", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+  }
+
+  p[_ngcontent-%COMP%] {
+    margin: 0;
+    color: var(--gray-700);
+  }
+
+  main[_ngcontent-%COMP%] {
+    width: 100%;
+    min-height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+    box-sizing: inherit;
+    position: relative;
+  }
+
+  .angular-logo[_ngcontent-%COMP%] {
+    max-width: 9.2rem;
+  }
+
+  .content[_ngcontent-%COMP%] {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    max-width: 700px;
+    margin-bottom: 3rem;
+  }
+
+  .content[_ngcontent-%COMP%]   h1[_ngcontent-%COMP%] {
+    margin-top: 1.75rem;
+  }
+
+  .content[_ngcontent-%COMP%]   p[_ngcontent-%COMP%] {
+    margin-top: 1.5rem;
+  }
+
+  .divider[_ngcontent-%COMP%] {
+    width: 1px;
+    background: var(--red-to-pink-to-purple-vertical-gradient);
+    margin-inline: 0.5rem;
+  }
+
+  .pill-group[_ngcontent-%COMP%] {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    flex-wrap: wrap;
+    gap: 1.25rem;
+  }
+
+  .pill[_ngcontent-%COMP%] {
+    display: flex;
+    align-items: center;
+    --pill-accent: var(--bright-blue);
+    background: color-mix(in srgb, var(--pill-accent) 5%, transparent);
+    color: var(--pill-accent);
+    padding-inline: 0.75rem;
+    padding-block: 0.375rem;
+    border-radius: 2.75rem;
+    border: 0;
+    transition: background 0.3s ease;
+    font-family: var(--inter-font);
+    font-size: 0.875rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.4rem;
+    letter-spacing: -0.00875rem;
+    text-decoration: none;
+  }
+
+  .pill[_ngcontent-%COMP%]:hover {
+    background: color-mix(in srgb, var(--pill-accent) 15%, transparent);
+  }
+
+  .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 1) {
+    --pill-accent: var(--bright-blue);
+  }
+  .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 2) {
+    --pill-accent: var(--french-violet);
+  }
+  .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 3), 
+   .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 4), 
+   .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 5) {
+    --pill-accent: var(--hot-red);
+  }
+
+  .pill-group[_ngcontent-%COMP%]   svg[_ngcontent-%COMP%] {
+    margin-inline-start: 0.25rem;
+  }
+
+  .social-links[_ngcontent-%COMP%] {
+    display: flex;
+    align-items: center;
+    gap: 0.73rem;
+    margin-top: 1.5rem;
+  }
+
+  .social-links[_ngcontent-%COMP%]   path[_ngcontent-%COMP%] {
+    transition: fill 0.3s ease;
+    fill: var(--gray-400);
+  }
+
+  .social-links[_ngcontent-%COMP%]   a[_ngcontent-%COMP%]:hover   svg[_ngcontent-%COMP%]   path[_ngcontent-%COMP%] {
+    fill: var(--gray-900);
+  }
+
+  @media screen and (max-width: 650px) {
+    .content[_ngcontent-%COMP%] {
+      flex-direction: column;
+      width: max-content;
+    }
+
+    .divider[_ngcontent-%COMP%] {
+      height: 1px;
+      width: 100%;
+      background: var(--red-to-pink-to-purple-horizontal-gradient);
+      margin-block: 1.5rem;
+    }
+  }`]})};var S=[];var O={providers:[_({eventCoalescing:!0}),y(S),w()]};var j={providers:[P()]},A=k(O,j);var G=()=>x(v,A),le=G;export{le as a};
--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SD3WYCDZ.mjs";import"./chunk-J4RVASKU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-KWN5H2S6.mjs";import"./chunk-J4RVASKU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
